0e06dbe46865d344fedfd40144c6e8dcfc43e816
CarlosMerino <jkl.merino@gmail.com>
2025-05-16 00:21:58 +0200

    WIP --> Mejorando el input

---
+++ SAPAssistant/wwwroot/js/textarea-resize.js
@@ -0,0 +1,17 @@
+﻿window.autoResize = (selector) => {
+    const textarea = document.querySelector(selector);
+    if (textarea)
+    {
+        textarea.style.height = 'auto';
+        textarea.style.height = `${ textarea.scrollHeight}
+        px`;
+    }
+};
+
+window.preventDefaultEnter = () => {
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter' && !event.shiftKey) {
+            event.preventDefault();
+        }
+    }, { once: true });
+};
