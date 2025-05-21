218953cb70ad708d657ef59433322249ad5ab5a4
CarlosMerino <jkl.merino@gmail.com>
2025-05-21 22:52:38 +0200

    WIP - Implementando mejoras en el asistente de chat - Separar los mensajes en componetes

---
+++ SAPAssistant/Models/Chat/ErrorMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace SAPAssistant.Models.Chat
+{
+    public class ErrorMessage : MessageBase
+    {
+        public string Error { get; set; } = string.Empty;
+        public override string Type => "Error";
+    }
+}
