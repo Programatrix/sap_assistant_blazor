c5dba7c3bdebf0f811956a232c6c1dab56772f10
CarlosMerino <jkl.merino@gmail.com>
2025-07-03 23:45:59 +0200

    WIP - Output chart

---
+++ SAPAssistant/Models/Chart/ChartConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace SAPAssistant.Models.Chart
+{
+    public class ChartConfig
+    {
+        public string XAxis { get; set; }
+        public string YAxis { get; set; }
+        public string ChartType { get; set; }
+    }
+
+}
